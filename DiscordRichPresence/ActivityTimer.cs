b0d771f0466ef3067ab2b1bf545d1d415d21aaf6
Rahul Menon <menonrahul02@gmail.com>
2020-08-08 14:51:53 -0500

    Create ActivityTimer class based on GameStateTimer

---
+++ DiscordRichPresence/ActivityTimer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordRichPresence
+{
+    class ActivityTimer
+    {
+        private readonly Predicate<GameScenes> precondictions;
+        private readonly Predicate<GameScenes> check;
+
+        public bool ActiveNow { get; private set; }
+        public long Timestamp { get; private set; }
+
+        public ActivityTimer(Predicate<GameScenes> precondictions, Predicate<GameScenes> check)
+        {
+            this.precondictions = precondictions;
+            this.check = check;
+            Timestamp = Utils.GetUnixTime();
+        }
+
+        public void Update()
+        {
+            var currentScene = HighLogic.LoadedScene;
+            bool active = precondictions.Invoke(currentScene) && check.Invoke(currentScene);
+
+            // Check if activity has changed status
+            if (active != ActiveNow)
+            {
+                ActiveNow = active;
+
+                if (active)
+                {
+                    Timestamp = Utils.GetUnixTime();
+                }
+            }
+        }
+    }
+}
---
+++ DiscordRichPresence/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordRichPresence
+{
+    class Utils
+    {
+        public static long GetUnixTime()
+        {
+            return (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+    }
+}

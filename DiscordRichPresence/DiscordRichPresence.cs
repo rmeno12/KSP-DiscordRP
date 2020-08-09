875086468b79547b9eed8c5bb6338df017cb2efb
Rahul Menon <menonrahul02@gmail.com>
2020-08-09 16:52:54 -0500

    Utils and addon name update

---
+++ DiscordRichPresence/DiscordRichPresence.cs
@@ -0,0 +1,86 @@
+﻿using Discord;
+using DiscordRichPresence.Activities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace DiscordRichPresence
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class DiscordRichPresence : MonoBehaviour
+    {
+        private Discord.Discord discord;
+        private ActivityManager activityManager;
+        private ActivityHolder activity;
+
+        private ActivityTracker activityTracker;
+
+        private float updateInterval = 10.0F; // Time between updates in seconds
+        private float lastUpdateTime = 0.0F;
+
+        private bool initialized;
+
+        void Awake()
+        {
+            lastUpdateTime = Time.time;
+            activity = new IdleActivity(Utils.GetUnixTime(), GameScenes.LOADING);
+        }
+
+        void Start()
+        {
+            Debug.Log("DiscordRichPresence: Plugin Startup");
+
+            discord = new Discord.Discord(386261941259337738, (ulong)CreateFlags.Default);
+            activityManager = discord.GetActivityManager();
+            activityTracker = new ActivityTracker();
+
+            DontDestroyOnLoad(this);
+
+            // TODO: Add pause detection/status update
+        }
+
+        void OnDisable()
+        {
+            Debug.Log("DiscordRichPresence: Plugin Disable");
+
+            initialized = false;
+        }
+
+        void Update()
+        {
+            activityTracker.UpdateTimers();
+
+            float currentTime = Time.time;
+
+            if (currentTime - lastUpdateTime > updateInterval || !initialized)
+            {
+                Debug.Log("DRP: check");
+                lastUpdateTime = currentTime;
+
+                UpdateRichPresence(activityTracker.GetCurrentActivityHolder());
+
+                initialized = true;
+            }
+        }
+
+        void UpdateRichPresence(ActivityHolder activityHolder)
+        {
+            ActivityHolder prev = activity;
+            activity = activityHolder;
+
+            if (!activity.Equals(prev) || !initialized)
+            {
+                activityManager.UpdateActivity(activity.GetActivity(), (result) => 
+                {
+                    if (result != Result.Ok)
+                    {
+                        Debug.Log(string.Format("DiscordRichPresence: UpdateActivity failed ({0})", result));
+                    }
+                });
+            }
+        }
+    }
+}
--- DiscordRichPresence/Utils.cs
+++ DiscordRichPresence/Utils.cs
@@ -12,5 +12,21 @@ namespace DiscordRichPresence
         {
             return (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
         }
+
+        public static string FormatDistance(double distance)
+        {
+            if (distance > 2000000)
+            {
+                return string.Format("{0:F0}Mm", distance / 1000000.0);
+            }
+            else if (distance > 2000)
+            {
+                return string.Format("{0:F0}km", distance / 1000.0);
+            }
+            else
+            {
+                return string.Format("{0:F0}m", distance);
+            }
+        }
     }
 }
